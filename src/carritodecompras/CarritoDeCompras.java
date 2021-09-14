0e7fec768e8091eaa9131f5ae32dbca8dc7b5a3b
fjml1983 <fjml1983@gmail.com>
2021-09-14 14:06:41 -0500

    Código actualizado a la fecha

--- src/carritodecompras/CarritoDeCompras.java
+++ src/carritodecompras/CarritoDeCompras.java
@@ -1,7 +1,12 @@
 package carritodecompras;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class CarritoDeCompras {
@@ -21,9 +26,19 @@ public class CarritoDeCompras {
         */
        
         /*Paradigma Funcional*/
+        
+       DoubleBinaryOperator suma = (acumulador , entrada) -> acumulador + entrada;
+        
        return productos.stream()
                 .mapToDouble(p -> p.getPrecio())
-                .sum();       
+                //.sum();       
+                //.reduce(0, Double::sum);
+                .reduce(0, suma);
+       
+                //  0 + 10 = 10
+                // 10 + 12 = 22
+                // 22 + 10 = 32
+                //...
     }    
     
     public List<Producto> filtrarProductosPorTipo(TipoProducto tipo){
@@ -37,18 +52,72 @@ public class CarritoDeCompras {
         return filtrados;       
         */
         
+        Predicate<Producto> predicado = p -> p.getTipo() == tipo;
+        
         /*Paradigma Funcional*/
         return productos.stream()
-                .filter(p -> p.getTipo() == tipo )
+                .filter(predicado)
                 .collect(Collectors.toList());
                               
     }    
 
     List<TipoProducto> obtenerTiposDeProductoDistintos() {
+        
+        Function<Producto, TipoProducto> mapper = p -> p.getTipo();
+        
         /*Paradigma Funcional*/
         return productos.stream()
-                .map(p -> p.getTipo())
+                //.peek(p -> System.out.println("Antes del map:" + p))
+                .map(mapper)
+                //.peek(tp -> System.out.println("Después del map, antes del disctinct:" + tp))
                 .distinct()
+                //.peek(tp -> System.out.println("Después del disctinct:" + tp))
                 .collect(Collectors.toList());
     }
+
+    List<Producto> obtenerProductosDistintos() {        
+        /*Paradigma Funcional*/
+        return productos.stream()                
+                .distinct()
+                .collect(Collectors.toList());        
+    }
+
+    List<Producto> obtenerProductosOrdenados() {
+        Comparator<Producto> comparador = 
+                Comparator.comparing(Producto::getNombre)
+                    .thenComparing(Producto::getPrecio);
+        
+        /*Paradigma Funcional*/
+        return productos.stream()
+                .sorted(comparador)
+                .collect(Collectors.toList());        
+    }
+
+    List<Producto> obtenerProductosAplicandoDescuento(int pctDescuento, TipoProducto tipo) {
+        return productos.stream()
+                .peek(p -> {if(p.getTipo()== tipo) p.setPrecio(p.getPrecio() * (100-pctDescuento)/100);})
+                .collect(Collectors.toList());        
+                
+    }
+
+    String obtenerProductosFormatoJSON() {
+        return productos.stream()
+                .map(p -> { return "{ nombre:" + p.getNombre() + ", precio:" + p.getPrecio() + ", tipo:" + p.getTipo() + "}"; })
+                .collect(Collectors.joining(","));        
+    }
+
+    Map<TipoProducto, List<Producto>> obtenerProductosAgrupadosPorTipo() {
+        return productos.stream()
+                .collect(Collectors.groupingBy(Producto::getTipo));
+    }
+
+    double obtenerPrecioPromedio() {
+        return productos.stream()
+                .collect(Collectors.averagingDouble(Producto::getPrecio))
+                .doubleValue();
+    }
+    
+       
+    
+    
 }
--- src/carritodecompras/Principal.java
+++ src/carritodecompras/Principal.java
@@ -5,19 +5,43 @@ public class Principal {
         CarritoDeCompras carrito = new CarritoDeCompras();
         
         carrito.agregarProducto(new Producto("Coca-Cola 350ml", 10, TipoProducto.BEBIDA ));
+        carrito.agregarProducto(new Producto("Coca-Cola 350ml", 12, TipoProducto.BEBIDA ));
+        carrito.agregarProducto(new Producto("Coca-Cola 350ml", 10, TipoProducto.BEBIDA ));
         carrito.agregarProducto(new Producto("Doritos Nachos 150g", 12, TipoProducto.FRITURA));
         carrito.agregarProducto(new Producto("Mirinda 350ml", 8.5, TipoProducto.BEBIDA ));
         carrito.agregarProducto(new Producto("Pollo fresco 500g", 50, TipoProducto.CARNES_FRIAS ));
+        carrito.agregarProducto(new Producto("Pollo fresco 500g", 50, TipoProducto.CARNES_FRIAS ));
         carrito.agregarProducto(new Producto("Libreta Prof. 100 hojas Scribe", 20.5, TipoProducto.PAPELERIA ));
-        carrito.agregarProducto(new Producto("Libreta Frac. 100 hojas Scribe", 25, TipoProducto.PAPELERIA ));
+        carrito.agregarProducto(new Producto("Libreta Fran. 90 hojas Scribe", 26, TipoProducto.PAPELERIA ));
+        carrito.agregarProducto(new Producto("Libreta Fran. 90 hojas Scribe", 25, TipoProducto.PAPELERIA ));
         
               
         System.out.println("Total del carrito: " + carrito.calcularTotal());                      
         
-        System.out.println("Los artículos de papelería en el carrito son: " + 
+        System.out.println("\nLos artículos de papelería en el carrito son: " + 
                             carrito.filtrarProductosPorTipo(TipoProducto.PAPELERIA));
         
-        System.out.println("Los tipos de producto distintos en el carrito son: " +
+        System.out.println("\nLos tipos de producto distintos en el carrito son: " +
                             carrito.obtenerTiposDeProductoDistintos());
+        
+        System.out.println("\nLos productos distintos en el carrito son: " +
+                            carrito.obtenerProductosDistintos());
+        
+        System.out.println("\nLos productos en el carrito de compras por orden, son: " + 
+                            carrito.obtenerProductosOrdenados());
+        
+        System.out.println("\nAplicar un 10% de descuento a los productos de PAPELERIA" +
+                            carrito.obtenerProductosAplicandoDescuento(10,TipoProducto.PAPELERIA));
+        
+        System.out.println("\nLa lista de productos de carrito en formato JSON: " +
+                            carrito.obtenerProductosFormatoJSON());
+        
+        
+        System.out.println("\nProductos del carrito de compras agrupados por tipo:" +
+                            carrito.obtenerProductosAgrupadosPorTipo());
+        
+        System.out.println("\nPrecio promedio de lo que llevamos en el carrito: " +
+                            carrito.obtenerPrecioPromedio());
+        
     }   
 }
